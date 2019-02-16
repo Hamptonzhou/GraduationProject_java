892550af72617ac9ed266effa30b98923eac3add
Hamptonzhou <Hamptonzhou@163.com>
2019-02-17 00:35:10 +0800

    自定义额外所需接口的dao层，一般用不到，使用Activiti提供的Service即可

---
+++ WorkflowSystem/src/main/java/com/southsmart/webplat/workflow/dao/CustomDao.java
@@ -0,0 +1,14 @@
+package com.southsmart.webplat.workflow.dao;
+
+/**
+ * 
+ * @Title:
+ * @Description:
+ * @Author:zhou
+ * @Since:2019年2月6日
+ * @Version:1.1.0
+ */
+//@Repository
+//public interface CustomDao extends JpaRepository<, > {
+//    
+//}
