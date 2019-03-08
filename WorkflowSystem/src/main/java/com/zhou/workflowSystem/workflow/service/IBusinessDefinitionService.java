e3834b4f2792cab13cb0e5fd6f10fc3ffc1e0450
hamptonzhou <hamptonzhou@163.com>
2019-03-08 22:23:28 +0800

    新增业务定义接口实现

---
+++ WorkflowSystem/src/main/java/com/zhou/workflowSystem/workflow/service/IBusinessDefinitionService.java
@@ -0,0 +1,40 @@
+package com.zhou.workflowSystem.workflow.service;
+
+import com.zhou.utils.PageQueryData;
+import com.zhou.workflowSystem.workflow.entity.BusinessDefinition;
+
+/**
+ * 
+ * @Title:
+ * @Description:
+ * @Author:zhou
+ * @Since:2019年3月7日
+ * @Version:1.1.0
+ */
+public interface IBusinessDefinitionService {
+    
+    /**
+     * 获取记录列表
+     * 
+     * @param pageQueryData
+     * @Description:
+     */
+    void getBusinessDefinitionList(PageQueryData<BusinessDefinition> pageQueryData);
+    
+    /**
+     * 新增或修改记录
+     * 
+     * @param businessDefinition
+     * @Description:
+     */
+    void saveOrUpdateBusinessDefinition(BusinessDefinition businessDefinition);
+    
+    /**
+     * 删除一条或多条记录
+     * 
+     * @param ids
+     * @Description:
+     */
+    void deleteMaterialByIds(Integer[] ids);
+    
+}
---
+++ WorkflowSystem/src/main/java/com/zhou/workflowSystem/workflow/service/impl/BusinessDefinitionServiceImpl.java
@@ -0,0 +1,50 @@
+package com.zhou.workflowSystem.workflow.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zhou.utils.PageQueryData;
+import com.zhou.workflowSystem.workflow.dao.BusinessDefinitionDao;
+import com.zhou.workflowSystem.workflow.entity.BusinessDefinition;
+import com.zhou.workflowSystem.workflow.service.IBusinessDefinitionService;
+
+/**
+ * 
+ * @Title:
+ * @Description:
+ * @Author:zhou
+ * @Since:2019年3月7日
+ * @Version:1.1.0
+ */
+@Service
+public class BusinessDefinitionServiceImpl implements IBusinessDefinitionService {
+    
+    @Autowired
+    private BusinessDefinitionDao businessDefinitionDao;
+    
+    @Override
+    public void getBusinessDefinitionList(PageQueryData<BusinessDefinition> pageQueryData) {
+        List<BusinessDefinition> list = businessDefinitionDao.findAll();
+        pageQueryData.setTotal(list.size());
+        pageQueryData.setQueryList(list);
+    }
+    
+    @Override
+    public void saveOrUpdateBusinessDefinition(BusinessDefinition businessDefinition) {
+        if (businessDefinition != null) {
+            businessDefinitionDao.save(businessDefinition);
+        }
+    }
+    
+    @Override
+    public void deleteMaterialByIds(Integer[] ids) {
+        for (Integer id : ids) {
+            BusinessDefinition businessDefinition = businessDefinitionDao.findById(id).orElse(null);
+            if (businessDefinition != null) {
+                businessDefinitionDao.deleteById(id);
+            }
+        }
+    }
+}
