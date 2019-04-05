02e3368a910be078810793dc1fccf5c65fa262a2
hamptonzhou <hamptonzhou@163.com>
2019-04-05 23:48:35 +0800

    added:创建者Id、是否启动字段

--- WorkflowSystem/src/main/java/com/zhou/workflowSystem/workflow/entity/BusinessDefinition.java
+++ WorkflowSystem/src/main/java/com/zhou/workflowSystem/workflow/entity/BusinessDefinition.java
@@ -16,29 +16,36 @@ import lombok.Data;
 @Data
 @DynamicUpdate
 public class BusinessDefinition {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    
+
     @Column(length = 50, nullable = false)
     private String businessName;
-    
+
     //业务使用的流程
     @Column(length = 50, nullable = false)
     private String processDefinitionId;
-    
+
     @Column(length = 50, nullable = false)
     private String processDefinitionName;
-    
+
     //业务使用的表单
     @Column(length = 50, nullable = false)
     private String businessFormId;
-    
+
     @Column(length = 50, nullable = false)
     private String businessFormName;
-    
+
     //备注信息，启动之后获得流程实例进行设置
     private String remarkContent;
-    
+
+    //业务的创建用户Id
+    private String creatorId;
+
+    //业务是否启动
+    private boolean started;
+
+
 }
