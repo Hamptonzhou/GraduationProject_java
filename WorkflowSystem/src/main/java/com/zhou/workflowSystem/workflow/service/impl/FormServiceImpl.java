92640f39f689d0c4c3243c05c96fbea1295faad5
hamptonzhou <hamptonzhou@163.com>
2019-04-21 17:12:13 +0800

    added:新增修改实体逻辑

--- WorkflowSystem/src/main/java/com/zhou/workflowSystem/workflow/service/impl/FormServiceImpl.java
+++ WorkflowSystem/src/main/java/com/zhou/workflowSystem/workflow/service/impl/FormServiceImpl.java
@@ -1,7 +1,13 @@
 package com.zhou.workflowSystem.workflow.service.impl;
 
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,7 +31,32 @@ public class FormServiceImpl implements IFormService {
     
     @Override
     public void saveOrUpdateFormData(BusinessFormData businessFormData) {
-        formDao.save(businessFormData);
+        if (businessFormData.getId() == null) {
+            formDao.save(businessFormData);
+        } else {
+            BusinessFormData EntityInDB = formDao.findById(businessFormData.getId()).orElse(null);
+            BeanUtils.copyProperties(businessFormData, EntityInDB, this.getNullPropertyNames(businessFormData));
+            formDao.save(EntityInDB);
+        }
+    }
+    
+    /**
+     * @param source
+     * @return 返回属性值为Null的属性名数组
+     * @Description: 用于实体拷贝时忽略值为Null属性
+     */
+    private String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        PropertyDescriptor[] pds = src.getPropertyDescriptors();
+        Set<String> emptyNames = new HashSet<String>();
+        for (PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) {
+                emptyNames.add(pd.getName());
+            }
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
     }
     
     @Override
