bbf2463067f45bda0c2a6e764e99e9e88138ff7f
hamptonzhou <hamptonzhou@163.com>
2019-04-21 15:57:06 +0800

    fixed:消息时间显示

--- MessageSystem/src/main/java/com/zhou/messageSystem/entity/MessageItem.java
+++ MessageSystem/src/main/java/com/zhou/messageSystem/entity/MessageItem.java
@@ -29,7 +29,7 @@ import lombok.Data;
 @Data
 @DynamicUpdate
 public class MessageItem {
-
+    
     /**
      * 消息id
      */
@@ -38,67 +38,68 @@ public class MessageItem {
     @GeneratedValue(generator = "AutoUUID")
     @Column(name = "id", length = 36)
     private String id;
-
+    
     /**
      * 发送者Id
      */
     @Column(name = "sender_id", length = 36)
     private String senderId;
-
+    
     /**
      * 发送者真实姓名
      */
     @Column(name = "sender_name", length = 30)
     private String senderName;
-
+    
     /**
      * 消息发送时间
      */
-
+    //@JsonFormat主要是后台到前台的时间格式的转换 ，@DataFormat主要是前后到后台的时间格式的转换，可能在修改的时候时间逻辑出现错误
+    @Column(name = "send_time", length = 30)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    private Date sendTime = new Date();
-
-
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss" )
+    private Date sendTime;
+    
     /**
      * 接收者Id，广播消息的接收者id设置为'#'
      */
     @Column(name = "receiver_id", length = 36)
     private String receiverId;
-
+    
     /**
      * 接收者真实姓名
      */
     @Column(name = "receiver_name", length = 30)
     private String receiverName;
-
+    
     /**
      * 消息标题
      */
     @Column(name = "title", length = 60)
     private String title;
-
+    
     /**
      * 消息内容
      */
     @Column(name = "content", length = 3000)
     private String content;
-
+    
     /**
      * 消息状态。0表示未读，1表示已读
      */
     @Column(name = "message_status")
     private int messageStatus;
-
+    
     /**
      * 消息类型。0---广播消息，1---个人消息
      */
     @Column(name = "type")
     private int type;
-
+    
     /**
      * 是否删除。0---未被删除，1---已被删除，用于区别哪些消息在回收站中
      */
     @Column(name = "is_delete")
     private int isDelete;
-
+    
 }
